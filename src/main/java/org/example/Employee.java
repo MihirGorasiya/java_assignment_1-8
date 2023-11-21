e6473a4fc34ac922079e0812f1c4da0ffac1bfee
Mihir Gorasiya <mihirgorasiya@gmail.com>
2023-11-21 16:06:49 -0500

    initial comment with basic code

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Employee {
+    private String name;
+    private int birthYear;
+    private int age;
+    private float monthlyIncome;
+    private float occupationRate;
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+
+}
+
+class Manager{
+
+}
+
+class Tester{
+
+}
+
+class Programmer{
+
+}
