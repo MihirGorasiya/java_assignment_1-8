3a0dd2770157ad60730a7ea42b466d7aaae6b407
Mihir Gorasiya <mihirgorasiya@gmail.com>
2023-11-21 16:26:06 -0500

    Employee class created

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,29 +1,72 @@
 package org.example;
 
+import java.util.Calendar;
+
+//Employee class initialization
 public class Employee {
-    private String name;
+    private final String employeeName;
     private int birthYear;
     private int age;
-    private float monthlyIncome;
-    private float occupationRate;
-    public void setName(String name){
-        this.name = name;
+    private double monthlyIncome;
+    private double occupationRate;
+
+    private boolean hasVehicle;
+
+
+    public Employee(String employeeName, int birthYear, double monthlyIncome, double occupationRate, boolean hasVehicle) {
+        this.employeeName = employeeName;
+        this.birthYear = birthYear;
+        this.age = Calendar.getInstance().get(Calendar.YEAR) - birthYear;
+        this.monthlyIncome = monthlyIncome;
+        this.occupationRate = occupationRate;
+        this.hasVehicle = hasVehicle;
     }
-    public String getName(){
-        return name;
+
+    //    name getter
+    public String getEmployeeName(){
+        return employeeName;
+    }
+//    birth year getter setter
+    public int getBirthYear(){
+        return birthYear;
     }
 
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
 
-}
+//   Age getter setter
+    public int getAge(){
+        return age;
+    }
 
-class Manager{
+    public void setAge() {
+        this.age = Calendar.getInstance().get(Calendar.YEAR) - this.birthYear;
+    }
+//   Monthly Income getter setter
+    public double getMonthlyIncome() {
+        return monthlyIncome;
+    }
 
-}
+    public void setMonthlyIncome(double monthlyIncome) {
+        this.monthlyIncome = monthlyIncome;
+    }
 
-class Tester{
+//   Occupation rate getter setter
+    public double getOccupationRate() {
+        return occupationRate;
+    }
 
-}
+    public void setOccupationRate(double occupationRate) {
+        this.occupationRate = occupationRate;
+    }
 
-class Programmer{
+//  has Vehicle getter setter
+    public boolean isHasVehicle() {
+        return hasVehicle;
+    }
 
+    public void setHasVehicle(boolean hasVehicle) {
+        this.hasVehicle = hasVehicle;
+    }
 }
