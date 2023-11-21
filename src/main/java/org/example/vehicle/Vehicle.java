d8f4ef6a886d658184d08d5c9f6b2402f351b8d6
Mihir Gorasiya <mihirgorasiya@gmail.com>
2023-11-21 17:29:02 -0500

    vehicle category enum added

---
+++ src/main/java/org/example/vehicle/Vehicle.java
@@ -0,0 +1,49 @@
+package org.example.vehicle;
+//  Vehicle class initialization
+public class Vehicle {
+    private String make;
+    private String plate;
+    private String color;
+    private VehicleCategory category;
+
+    public Vehicle(String make, String plate, String color, VehicleCategory category) {
+        this.make = make;
+        this.plate = plate;
+        this.color = color;
+        this.category = category;
+    }
+    //  Make getter setter
+    public String getMake() {
+        return make;
+    }
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    //  plate getter setter
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    //  color getter setter
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    //  category getter setter
+    public VehicleCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(VehicleCategory category) {
+        this.category = category;
+    }
+}
---
+++ src/main/java/org/example/vehicle/VehicleCategory.java
@@ -0,0 +1,3 @@
+package org.example.vehicle;
+
+public enum VehicleCategory {FAMILY, SPORT}
